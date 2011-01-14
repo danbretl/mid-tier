4b45abbfda43c526a759c714c6b6a26faded658b
Pasha Katsev <pkatsev@abextratech.com>
2011-01-14 12:18:37 -0500

    added a basic auth and an Event handler

---
+++ abextra/api/handlers.py
@@ -0,0 +1,14 @@
+from piston.handler import BaseHandler
+from events.models import Event
+
+class EventHandler(BaseHandler):
+   allowed_methods = ('GET',)
+   model = Event
+
+   def read(self, request, event_id=None):
+        """
+        Returns a single event if 'event_id' is given,
+        otherwise a subset.
+        """
+        m = Event.objects
+        return m.get(pk=event_id) if event_id else m.all()
--- abextra/api/urls.py
+++ abextra/api/urls.py
@@ -1,5 +1,16 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
+from piston.resource import Resource
+from piston.authentication import HttpBasicAuthentication
 
+from api.handlers import EventHandler
+
+auth = HttpBasicAuthentication()
+ad = { 'authentication': auth }
+
+event_resource = Resource(handler=EventHandler, **ad)
+# event_resource = Resource(handler=EventHandler)
+
+urlpatterns = patterns('',
+    url(r'^events/(?P<event_id>[^/]+)/$', event_resource),
 )
