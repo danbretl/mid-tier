6e63200a97c4684b448ba0565291d2631ac0cddf
Pasha Katsev <pkatsev@abextratech.com>
2011-01-17 17:44:02 -0500

    removing scrape db schema for now

--- abextra/settings.py
+++ abextra/settings.py
@@ -26,14 +26,14 @@ DATABASES = {
         'HOST': 'localhost',                    # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     },
-    'scrape': {
-        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'scrape',                       # Or path to database file if using sqlite3.
-        'USER': 'abex_dev',                        # Not used with sqlite3.
-        'PASSWORD': 'abex113',                  # Not used with sqlite3.
-        'HOST': 'testsv.abextratech.com',                    # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
-    }
+    # 'scrape': {
+    #     'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': 'scrape',                       # Or path to database file if using sqlite3.
+    #     'USER': 'abex_dev',                        # Not used with sqlite3.
+    #     'PASSWORD': 'abex113',                  # Not used with sqlite3.
+    #     'HOST': 'testsv.abextratech.com',                    # Set to empty string for localhost. Not used with sqlite3.
+    #     'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
+    # }
 }
 
 # Local time zone for this installation. Choices can be found here:
