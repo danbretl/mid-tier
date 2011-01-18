d4855f9a1e5f193e90255fb1c4824154ab68677e
Pasha Katsev <pkatsev@abextratech.com>
2011-01-18 15:22:59 -0500

    fixed mgmnt command

--- abextra/events/management/commands/loadscraped.py
+++ abextra/events/management/commands/loadscraped.py
@@ -1,14 +1,12 @@
-from django.core.management.base import BaseCommand, CommandError
-from events.models import ScrapedEvent
+from django.core.management.base import NoArgsCommand
+from abextra.events.models import ScrapedEvent
 
-class Command(BaseCommand):
-    # args = '<poll_id poll_id ...>'
+class Command(NoArgsCommand):
     help = 'Loads scraped events from the scraped view'
 
-    def handle(self, *args, **options):
+    def handle(self, **options):
         try:
             for e in ScrapedEvent.objects.all(): e.to_event()
         except e:
             raise CommandError(e)
-
         self.stdout.write('Successfully loaded scraped events')
