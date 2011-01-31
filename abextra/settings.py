0ff84df306747ef3196967d83d4a868cccbbf5a3
Pasha Katsev <pkatsev@abextratech.com>
2011-01-31 11:21:46 -0500

    aggregate signal is complete and tested

--- abextra/settings.py
+++ abextra/settings.py
@@ -83,9 +83,11 @@ SECRET_KEY = '3xh0mi2n)pvri!l^-8-@-xkjn^uc#q!79!yfdc-@qe&!4e4_em'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+        #     'django.template.loaders.eggs.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
