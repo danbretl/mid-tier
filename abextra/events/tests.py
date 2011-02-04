248dfcbe6af3664bd280a11817c0366069e95203
Pasha Katsev <pkatsev@abextratech.com>
2011-02-04 14:12:23 -0500

    starting work on tests, cached category tree

--- abextra/events/tests.py
+++ abextra/events/tests.py
@@ -5,8 +5,29 @@ class SimpleTest(TestCase):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        self.failUnlessEqual(1 + 1, 2
 
+
+# -------------------------------------------------------------------------
+import random
+
+from events import models as ems
+class EventureInitializer(object):
+    """factory class for initialization of eventure app"""
+
+    def __init__(self, n_events=100, categories=None, cross_categorization=4):
+        super(EventureInitializer, self).__init__()
+        self.n_events = n_events
+
+        # fetch necessary categories
+        c_base = ems.Category.objects
+        self.categories = c_base.filter(id__in=categories) if categories else c_base.all()
+
+    def run(self):
+        pass
+
+from core.utils import Bunch
+CategoryNode = Bunch
 
 # -------------------------------------------------------------------------
 import random
@@ -38,5 +59,5 @@ for id, category in categories_by_id.iteritems():
 
     # assign children
     category.children = category_by_id
-    
+
     
