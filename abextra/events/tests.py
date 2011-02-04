6d634113e2d7791de0a7be0df98762d0abdd7213
Pasha Katsev <pkatsev@abextratech.com>
2011-02-04 14:12:23 -0500

    starting work on tests, cached category tree

--- abextra/events/tests.py
+++ abextra/events/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 class SimpleTest(TestCase):
@@ -12,12 +5,38 @@ class SimpleTest(TestCase):
         """
         Tests that 1 + 1 always equals 2.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        self.failUnlessEqual(1 + 1, 2
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+# -------------------------------------------------------------------------
+import random
 
+from events import models as ems
+class EventureInitializer(object):
+    """factory class for initialization of eventure app"""
+
+    def __init__(self, n_events=100, categories=None, cross_categorization=4):
+        super(EventureInitializer, self).__init__()
+        self.n_events = n_events
+
+        # fetch necessary categories
+        c_base = ems.Category.objects
+        self.categories = c_base.filter(id__in=categories) if categories else c_base.all()
+
+    def run(self):
+        pass
+
+from core.utils import Bunch
+CategoryNode = Bunch
+
+categories_by_id = dict((c.id, c) for c in Category.objects.all())
+
+for id, category in categories_by_id.iteritems():
+    # assign parent from the cache hash
+    parent_id = category.parent_id
+    category.parent = categories_by_id[parent_id] if parent_id else None
+
+    # assign children
+    category.children = category_by_id
+    
+    
