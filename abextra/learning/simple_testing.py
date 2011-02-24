b0dbd07095f492b66a643769d850979fda01983e
Vikas Menon <vmenon@abextratech.com>
2011-02-24 10:26:21 -0500

    simple tests with Category Manager

--- abextra/learning/simple_testing.py
+++ abextra/learning/simple_testing.py
@@ -1,3 +1,4 @@
+"""
 from learning import TestingFramework
 from django.contrib.auth.models import User
 
@@ -5,4 +6,10 @@ u = User.objects.get(id=1)
 c = TestingFramework.EventureUser(u,categories=['Bars','Clubs','Musical','Poetry'])
 #c = TestingFramework.EventureUser(u,categories=['Museum','Daily Show'])
 c.calculate_plot_metrics(50)
+"""
 
+
+from events.models import Event, CategoryManager, Category
+a = CategoryManager()
+e = Event.objects.filter(id__lt=10)
+dictionary = a.for_events(e)
