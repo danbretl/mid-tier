5d60a4288d200158725cca8066a9fb76a6d6f740
Pasha Katsev <pkatsev@abextratech.com>
2011-02-27 02:18:01 -0500

    autocompletion for admin ext category mapping

--- abextra/preprocess/admin.py
+++ abextra/preprocess/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+
+from autocomplete.views import autocomplete, AutocompleteSettings
+from autocomplete.admin import AutocompleteAdmin
+
 from preprocess import models, forms
+from events.models import Category
 
 class ScrapedEventAdmin(admin.ModelAdmin):
     list_display = ('title',)
@@ -10,8 +15,17 @@ class SourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'domain')
 admin.site.register(models.Source, SourceAdmin)
 
-class ExternalCategoryAdmin(admin.ModelAdmin):
-    form = forms.ExternalCategoryAdminForm
+
+external_categories_autocomplete_settings = AutocompleteSettings
+external_categories_autocomplete_settings.login_required = True
+
+class ExternalCategoryAutocomplete(external_categories_autocomplete_settings):
+    queryset = Category.objects.filter(category_type__in='CA')
+    search_fields = ('^title',)
+autocomplete.register(models.ExternalCategory.category, ExternalCategoryAutocomplete)
+
+class ExternalCategoryAdmin(AutocompleteAdmin,admin.ModelAdmin):
+    # form = forms.ExternalCategoryAdminForm
     fields = ('name', 'xid', 'source', 'category')
     list_display = ('name', 'xid', 'source', 'category')
     list_filter = ('source',)
