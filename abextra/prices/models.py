cf6dd1d1678107c8feff35de4d8e2cb3e5b177fe
Pasha Katsev <pkatsev@abextratech.com>
2011-03-04 16:16:58 -0500

    prices model

--- abextra/prices/models.py
+++ abextra/prices/models.py
@@ -5,7 +5,7 @@ class Price(models.Model):
     quantity = models.FloatField(null=True, blank=True)
     units = models.CharField(max_length=300, blank=True)
     remark = models.CharField(max_length=300, blank=True)
-    occurrence_id = models.IntegerField(null=True, blank=True)
+    occurrence = models.ForeignKey(reverse)
     objects = MGR()
     class Meta:
         db_table = u'price'
