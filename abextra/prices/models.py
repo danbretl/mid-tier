c1968f6359bf725a3fc9af3d4e62bf57fae03685
Pasha Katsev <pkatsev@abextratech.com>
2011-03-07 10:36:42 -0500

    working on prices models

--- abextra/prices/models.py
+++ abextra/prices/models.py
@@ -2,11 +2,7 @@ from django.db import models
 from events.models import Occurrence
 
 class Price(models.Model):
-    quantity = models.FloatField(null=True, blank=True)
-    units = models.CharField(max_length=300, blank=True)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2);
+    units = models.CharField(max_length=300, default='dollars')
     remark = models.CharField(max_length=300, blank=True)
-    occurrence = models.ForeignKey(reverse)
-    objects = MGR()
-    class Meta:
-        db_table = u'price'
-        managed = False
+    occurrence = models.ForeignKey(related_name='prices')
