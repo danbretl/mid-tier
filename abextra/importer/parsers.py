8aa82609e662c5fa3c48f0fa83e68c36989e6791
Pavel Katsev <pkatsev@gmail.com>
2011-03-26 04:02:13 -0400

    chipping away at the importer

--- abextra/importer/parsers.py
+++ abextra/importer/parsers.py
@@ -1,41 +1,56 @@
-from importer import parser_registry
-from parser_base import BaseParser
+from collections import namedtuple
 from events.forms import CategoryImportForm
+from places.forms import PlaceImportForm, PointImportForm, CityImportForm
 
 class BaseParser(object):
-    def __init__(self, obj_dict):
-        self.obj_dict = obj_dict
+    def __init__(self):
+        self.model = self.model_form._meta.model
+        self.key_tuple = namedtuple('key_tuple', self.fields)
+        self.cache = {}
 
-    def parse(self):
-        form_data = self.parse_form_data()
-        form = self.model_form(data=form_data)
-        if form.is_valid():
-            return form.instance
+    def parse(self, data):
+        form_data = self.parse_form_data(data)
+        key = self.cache_key(form_data)
+        instance = self.cache.get(key)
+        if not instance:
+            try:
+                instance = self.model.objects.get(**key._asdict())
+                self.cache[key] = instance
+            except self.model.DoesNotExist:
+                form = self.model_form(form_data)
+                # this will raise a ValueError, if not valid
+                instance = form.save(commit=True)
+        return instance
 
-    def parse_form_data(self):
+    def cache_key(self, form_data):
+        return self.key_tuple(
+            **dict((f, form_data[f]) for f in self.fields if form_data.has_key(f))
+        )
+
+    def parse_form_data(self, obj_dict):
         raise NotImplementedError()
 
+class CityParser(BaseParser):
+    model_form = CityImportForm
+    fields = ['city', 'state']
 
-class CategoryParser(BaseParser):
-    model_form = CategoryImportForm
-    def parse_form_data(self):
-        return self.obj_dict
-parser_registry.register('category', CategoryParser)
+    def parse_form_data(self, data):
+        form_data = {}
+        form_data['city'] = data['city']
+        form_data['state'] = data['state']
+        return form_data
 
+class PointParser(BaseParser):
+    model_form = PointImportForm
+    fields = ['latitude', 'longitude', 'address']
+    city_parser = CityParser()
 
-class EventParser(BaseParser):
-    def parse_form_data(self):
-        return self.obj_dict
-parser_registry.register('event', EventParser)
-
-
-class OccurrenceParser(BaseParser):
-    def parse_form_data(self):
-        return self.obj_dict
-parser_registry.register('occurrence', OccurrenceParser)
-
-
-class LocationParser(BaseParser):
-    def parse_form_data(self):
-        return self.obj_dict
-parser_registry.register('location', LocationParser)
+    def parse_form_data(self, data):
+        form_data = {}
+        form_data['latitude'] = data['latitude']
+        form_data['longitude'] = data['longitude']
+        form_data['address'] = data['address']
+        form_data['zip'] = data['zipcode']
+        form_data['city'] = self.city_parser.parse(data).id
+        form_data['country'] = 'US'
+        return form_data
--- abextra/places/forms.py
+++ abextra/places/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.localflavor.us import forms as us_forms
+from django.template.defaultfilters import slugify
 from places.models import Place, Point, City
 
 class PlaceForm(forms.ModelForm):
@@ -13,12 +14,15 @@ class PointForm(forms.ModelForm):
 
 class CityForm(forms.ModelForm):
     state = us_forms.USStateField()
+    slug = forms.SlugField(required=False)
+
+    def clean_slug(self):
+        city = self.cleaned_data['city']
+        state = self.cleaned_data['state']
+        return slugify(u'-'.join((city, state)))
+
     class Meta:
         model = City
-
-# class ValidationMixin(forms.ModelForm):
-#     def is_valid():
-#         
 
 class PlaceImportForm(PlaceForm):
     pass
