22af212f95c4f1d90e0bd32f6582cdb67033b33b
Pavel Katsev <pkatsev@gmail.com>
2011-04-04 11:45:17 -0700

    better oo refactoring + images fix

--- abextra/importer/parsers/base.py
+++ abextra/importer/parsers/base.py
@@ -7,19 +7,19 @@ class BaseParser(object):
         self.cache = {}
 
     def parse(self, data):
-        form_data, file_data = self.parse_form_data(data)
+        form_data = self.parse_form_data(data, {})
         key = self.cache_key(form_data)
         created, instance = False, self.cache.get(key)
         if not instance:
             try:
                 created, instance = False, self.model.objects.get(**key._asdict())
             except self.model.DoesNotExist:
+                file_data = self.parse_file_data(data, {})
                 form = self.model_form(data=form_data, files=file_data)
                 if form.is_valid():
                     created, instance = True, form.save(commit=True)
                 else:
                     print form.errors
-                    # import ipdb; ipdb.set_trace()
             if instance:
                 self.cache[key] = instance
         return created, instance
@@ -29,5 +29,8 @@ class BaseParser(object):
             **dict((f, form_data[f]) for f in self.fields if form_data.has_key(f))
         )
 
-    def parse_form_data(self, obj_dict):
+    def parse_form_data(self, obj_dict, form_data={}):
         raise NotImplementedError()
+
+    def parse_file_data(self, obj_dict, file_data={}):
+        return
