5e858f8ab0cc1732e65df6faa94cf595e61e8e56
Pavel Katsev <pkatsev@gmail.com>
2011-04-12 17:34:54 -0400

    minor tweaks + data

--- abextra/events/models.py
+++ abextra/events/models.py
@@ -65,6 +65,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = _('categories')
+        ordering = ['title']
 
     def __unicode__(self):
         return self.title
