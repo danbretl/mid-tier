79b8fbaf04bc4fdf8771de74f6a057bf3fc6cec9
Pavel Katsev <pkatsev@gmail.com>
2011-04-20 12:33:05 -0400

    fix for ext_category admin | no autocomplete, per vikas' request

--- abextra/importer/admin.py
+++ abextra/importer/admin.py
@@ -7,21 +7,19 @@ from importer.models import ExternalCategory, RegexCategory
 from importer.forms import ExternalCategoryAdminForm
 from events.models import Category
 
-class ExternalCategoryAutocomplete(AutocompleteSettings):
-    login_required = True
-    queryset = Category.objects.filter(category_type__in='CA')
-    search_fields = ('^title',)
-autocomplete.register(ExternalCategory.category, ExternalCategoryAutocomplete)
+# class ExternalCategoryAutocomplete(AutocompleteSettings):
+#     login_required = True
+#     queryset = Category.objects.filter(category_type__in='CA')
+#     search_fields = ('^title',)
+# autocomplete.register(ExternalCategory.category, ExternalCategoryAutocomplete)
 
-class ExternalCategoryAdmin(AutocompleteAdmin, admin.ModelAdmin):
-    # form = ExternalCategoryAdminForm
+# class ExternalCategoryAdmin(AutocompleteAdmin, admin.ModelAdmin):
+class ExternalCategoryAdmin(admin.ModelAdmin):
+    form = ExternalCategoryAdminForm
     list_select_related = True
     list_display = ('name', 'xid', 'source', 'category_title')
     fields = ('name', 'xid', 'source', 'category')
     list_filter = ('source',)
-
-    # def category(self, external_category):
-    #     return external_category.category.title
 
 admin.site.register(ExternalCategory, ExternalCategoryAdmin)
 
@@ -32,5 +30,5 @@ class SourceRegexAdmin(admin.ModelAdmin):
     fields = ('source', 'regex', 'category', 'model_type')
     list_filter = ('source',)
     search_fields = ('source',)
-    
+
 admin.site.register(RegexCategory, SourceRegexAdmin)
