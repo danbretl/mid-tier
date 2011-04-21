763c8aad31578ddb475799defcad5dfe4e512a36
Pavel Katsev <pkatsev@gmail.com>
2011-04-21 09:40:52 -0400

    test tweaking :: eager loading issues

--- abextra/importer/parsers/tests.py
+++ abextra/importer/parsers/tests.py
@@ -4,62 +4,64 @@ from importer.parsers.locations import CityParser, PointParser, PlaceParser
 from importer.parsers.event import OccurrenceParser, EventParser, ExternalCategoryParser
 from importer.parsers.price import PriceParser
 
-class ExternalCategoryParserTest(TestCase):
-    fixtures = ['sources'] # ,'external_categories']
-    # consumer = ScrapeFeedConsumer()
-    parser = ExternalCategoryParser()
-
-    def test_parse(self):
-        for category in self.consumer.categories:
-            self.parser.parse(category)
-
-class CityParserTest(TestCase):
-    # consumer = ScrapeFeedConsumer()
-    parser = CityParser()
-
-    def test_parse(self):
-        for location in self.consumer.locations:
-            self.parser.parse(location)
-
-class PointParserTest(TestCase):
-    # consumer = ScrapeFeedConsumer()
-    parser = PointParser()
-
-    def test_parse(self):
-        for location in self.consumer.locations:
-            self.parser.parse(location)
-
-class PlaceParserTest(TestCase):
-    # consumer = ScrapeFeedConsumer()
-    parser = PlaceParser()
-
-    def test_parse(self):
-        for location in self.consumer.locations:
-            self.parser.parse(location)
-
-class PriceParserTest(TestCase):
-    fixtures = ['events']
-    # consumer = ScrapeFeedConsumer()
-    parser = PriceParser()
-
-    def test_parse(self):
-        for price in self.consumer.prices:
-            price['occurrence'] = 1
-            self.parser.parse(price)
-
-class OccurrenceParserTest(TestCase):
-    fixtures = ['events']
-    # consumer = ScrapeFeedConsumer()
-    parser = OccurrenceParser()
-
-    def test_parse(self):
-        for occurrence in self.consumer.occurrences:
-            self.parser.parse(occurrence)
+# class ExternalCategoryParserTest(TestCase):
+#     fixtures = ['sources'] # ,'external_categories']
+#     # consumer = ScrapeFeedConsumer()
+#     parser = ExternalCategoryParser()
+# 
+#     def test_parse(self):
+#         for category in self.consumer.categories:
+#             self.parser.parse(category)
+# 
+# class CityParserTest(TestCase):
+#     # consumer = ScrapeFeedConsumer()
+#     parser = CityParser()
+# 
+#     def test_parse(self):
+#         for location in self.consumer.locations:
+#             self.parser.parse(location)
+# 
+# class PointParserTest(TestCase):
+#     # consumer = ScrapeFeedConsumer()
+#     parser = PointParser()
+# 
+#     def test_parse(self):
+#         for location in self.consumer.locations:
+#             self.parser.parse(location)
+# 
+# class PlaceParserTest(TestCase):
+#     # consumer = ScrapeFeedConsumer()
+#     parser = PlaceParser()
+# 
+#     def test_parse(self):
+#         for location in self.consumer.locations:
+#             self.parser.parse(location)
+# 
+# class PriceParserTest(TestCase):
+#     fixtures = ['events']
+#     # consumer = ScrapeFeedConsumer()
+#     parser = PriceParser()
+# 
+#     def test_parse(self):
+#         for price in self.consumer.prices:
+#             price['occurrence'] = 1
+#             self.parser.parse(price)
+# 
+# class OccurrenceParserTest(TestCase):
+#     fixtures = ['events']
+#     # consumer = ScrapeFeedConsumer()
+#     parser = OccurrenceParser()
+# 
+#     def test_parse(self):
+#         for occurrence in self.consumer.occurrences:
+#             self.parser.parse(occurrence)
 
 class EventParserTest(TestCase):
     fixtures = ['auth', 'categories', 'sources', 'external_categories']
-    consumer = ScrapeFeedConsumer()
-    parser = EventParser()
+
+    def setUp(self):
+        self.consumer = ScrapeFeedConsumer()
+        self.parser = EventParser()
 
     def test_parse(self):
         for event in self.consumer.events():
