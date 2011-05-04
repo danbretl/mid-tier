da77dd7857235eba909cfd9d61856b2c6952d989
Vikas Menon <menonvikas@gmail.com>
2011-05-04 16:52:45 -0400

    fully copied haystack branch

---
+++ abextra/events/search_indexes.py
@@ -0,0 +1,18 @@
+from haystack import indexes
+from haystack import site
+from events.models import Event
+
+class EventIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    # Regarding boosting: http://readthedocs.org/docs/django-haystack/v1.1/boost.html#field-boost.
+    # FIXME: This however does not work with the current version.
+    title = indexes.CharField(model_attr='title')#, boost=1.5)
+    description = indexes.CharField(model_attr='description')
+
+    def get_queryset(self):
+        """Used when the entire index for model is updated."""
+        # We also want to index past events in case a user wants to find
+        # older events.
+        return Event.objects.all()
+
+site.register(Event, EventIndex)
