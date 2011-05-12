6543a9861556e82d0b0d23864cb58b1a038791ca
Vikas Menon <menonvikas@gmail.com>
2011-05-12 16:31:13 -0400

    raising exception for invalid parameters

--- abextra/events/resources.py
+++ abextra/events/resources.py
@@ -191,20 +191,22 @@ class EventRecommendationResource(EventSummaryResource):
         if cpc_filter:
             orm_filters.update(summary__concrete_parent_category=cpc_filter)
 
-        event_qs = None
-        if 'view' in filters:
-            if filters['view'] == 'popular':
-                events_qs = Event.active.future().filter(**orm_filters).order_by('-popularity_score')[:100]
-            elif filters['view'] == 'free':
+        events_qs = Event.active.future().filter(**orm_filters)
+        view = filters.get('view')
+        if view:
+            if view == 'popular':
+                events_qs = events_qs.order_by('-popularity_score')[:100]
+            elif view == 'free':
                 ids = EventSummary.objects.filter(price_quantity_max=0).values_list('event',
                                                                      flat=True)
-                events_qs = Event.active.future().filter(**orm_filters).filter(id__in=ids)[:100]
+                events_qs = events_qs.filter(id__in=ids)[:100]
             else:
-                events_qs = Event.objects.none()
+                msg = "Invalid value for get parameter 'view'"
+                response = HttpBadRequest(content=msg)
+                raise ImmediateHttpResponse(response=response)
         else:
             # filter initial event queryset
-            events_qs = Event.active.future().filter(**orm_filters) \
-                        .filter_user_actions(request.user, 'VI')
+            events_qs = events_qs.filter_user_actions(request.user, 'VI')
 
         # use machine learning
         recommended_events = ml.recommend_events(request.user, events_qs)
