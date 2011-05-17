f068850c54b91bd9eb02c838e17e7f6f825fd616
Vikas Menon <menonvikas@gmail.com>
2011-05-17 16:51:06 -0400

    fixing older learning/ paths to thor/

--- abextra/thor/testing_framework.py
+++ abextra/thor/testing_framework.py
@@ -21,7 +21,7 @@ class EventureUser:
     """
     This class creates a new user for ML testing every time.
     """
-    
+
     def __init__(self, user=None, categories=None, num_categories=None):
         """
         Initialize a new user and prepare to get recommendations from scratch.
@@ -33,8 +33,8 @@ class EventureUser:
             self.delete_user = False
         else:
             # Create and assign a new user.
-            # if necessary later delete the user as well. But this might not be 
-            # necessary since in Django we will be working with a fresh database 
+            # if necessary later delete the user as well. But this might not be
+            # necessary since in Django we will be working with a fresh database
             # that will in any case be destructed at the end of testing.
             self.delete_user = True
             success = False
@@ -42,13 +42,13 @@ class EventureUser:
             while not success:
                 count += 1
                 try:
-                    self.user = User(username="test"+str(count), 
+                    self.user = User(username="test"+str(count),
                                      password='test'+str(count))
                     self.user.save()
                     success = True
                 except:
                     success = False
-                    
+
         for c in Category.objects.all():
             try:
                 #See if event action aggregate exists.If it does, reset it to default.
@@ -56,15 +56,15 @@ class EventureUser:
             except:
                 #Else create one.
                 eaa = EventActionAggregate(user=self.user, category=c)
-            
+
             eaa.g, eaa.v, eaa.i, eaa.x = 0, 0, 0, 0
             eaa.save()
-        
+
         if categories:
             categories = [self.get_category_id(c) for c in categories]
             self.preferred_categories = set([c for c in categories])
         else:
-            self.preferred_categories = set([c.id for c in 
+            self.preferred_categories = set([c.id for c in
                                              self.get_random_categories(num_categories)])
 
     def get_category_id(self,string):
@@ -90,7 +90,7 @@ class EventureUser:
         """
         #import pdb; pdb.set_trace()
         #events = Event.objects.in_bulk(event_ids)
-        #recommended_categories = [a for b in recommended_categories for a in b]            
+        #recommended_categories = [a for b in recommended_categories for a in b]
         for categories in category_ids:
             # if there is an intersection with preferred categories
             if self.preferred_categories.intersection(set(categories)):
@@ -98,14 +98,14 @@ class EventureUser:
                     self.update_user_category_behavior(c,(1,0,0,0))
             else:
                 #This was a bad recommendation, so X it.
-                #if random.random() < 0.15 :                            
+                #if random.random() < 0.15 :
                 # Roughly 15% of the time we delete a category.
-                for c in categories: 
+                for c in categories:
                     self.update_user_category_behavior(c,(0,0,0,1))
 
     def update_user_category_behavior(self,category_id,(g,v,i,x)=(0,0,0,0)):
         """change the user's aggregate action by given (g,v,i,x) tuple"""
-        eaa = EventActionAggregate.objects.get(user=self.user, 
+        eaa = EventActionAggregate.objects.get(user=self.user,
                                                category__id=category_id)
         eaa.g += g
         eaa.v += v
@@ -127,8 +127,8 @@ class EventureUser:
 
     def calculate_recall_value(self, event_category_ids, event_ids=None):
         """
-        Return the recall between the recommended events given and this user's 
-        preferred events. The recall is the % of types of events that the user 
+        Return the recall between the recommended events given and this user's
+        preferred events. The recall is the % of types of events that the user
         likes that appear in the recommendations
         """
         recommended_categories = set([a for b in event_category_ids for a in b])
@@ -144,7 +144,7 @@ class EventureUser:
         Return the precision between the recommended events given and the
         user's preferred categories. The precision is the % of recommendations
         that the user likes."""
-        result = [len(set(c).intersection(self.preferred_categories))>0 
+        result = [len(set(c).intersection(self.preferred_categories))>0
                     for c in event_category_ids]
         #print "precision result: ", result
         return (sum(result) * 100.0 / len(result),result)
@@ -157,7 +157,7 @@ class EventureUser:
             mean = 0.0
         rms_value = math.sqrt(sum( [(x-mean)*(x-mean) for x in lst]))
         return rms_value
-                              
+
     def plot_with_error_bars(self,values_list=None):
         mean = [sum(x) * 1.0 / len(x) for x in izip(*values_list)]
         error_range_lower = [(sum(x) * 1.0 / len(x)) - min(x) for x in izip(*values_list)]
@@ -193,7 +193,7 @@ class EventureUser:
             plt.xlabel("Trials")
             plt.ylabel("Average presence in Recommendations")
             self.plot_with_error_bars([precision[0] for precision in precision_recall])
-            plt.savefig("learning/test_results/precision." + str(j) +".pdf")
+            plt.savefig("thor/test_results/precision." + str(j) +".pdf")
             plt.cla()
             # Plot recall
             plt.figure(2)
@@ -201,11 +201,11 @@ class EventureUser:
             plt.xlabel("Trials")
             plt.ylabel("% of User preferred categories")
             self.plot_with_error_bars([recall[1] for recall in precision_recall])
-            plt.savefig("learning/test_results/recall." + str(j) +".pdf")
+            plt.savefig("thor/test_results/recall." + str(j) +".pdf")
             plt.cla()
-            
+
         self.preferred_categories = set(preferred_categories)
-    
+
     def calculate_plot_metrics(self, number_of_recommendations=1):
         """
         This method calculates and plots (currently precision and recall) metrics.
@@ -217,20 +217,20 @@ class EventureUser:
         for i in range(number_of_recommendations):
             print "In loop: ", i, "\r",
             sys.stdout.flush()
-            
+
             #event_ids = [e.id for e in ml.recommend_events(self.user)]
             #event_category_id_dict = ml.get_categories(event_ids,'C')
             #event_category_ids = [event_category_id_dict[e] for e in event_ids]
-            
+
             # just get category ids directly
-            
+
             cats = ml.recommend_categories(self.user)
-            event_categories = ml.sample_category_distribution(cats.items(), 
+            event_categories = ml.sample_category_distribution(cats.items(),
                                                     settings.N)
 
             event_categories= [[a.id] for a in event_categories]
             #print map(lambda l: map(self.get_category_string, l),event_category_ids)
-            
+
             p,pres =self.calculate_precision_value(event_categories)
             precision.append(p)
             precision_set.append(pres)
@@ -253,4 +253,3 @@ class EventureUser:
         #plt.figure(2)
         #plt.plot(recall,color=color, label=label)
         return (precision, recall)
-
