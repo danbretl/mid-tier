8613d174628d8ba772a393999e0bd59c6251f299
Vikas Menon <menonvikas@gmail.com>
2011-06-29 00:01:18 -0400

    adding categories to places for use for categorization. Changes to events.models have been made to prevent circular imports

--- abextra/events/models.py
+++ abextra/events/models.py
@@ -122,6 +122,7 @@ class EventActiveManager(EventManager):
 
 class Event(models.Model):
     """Event model"""
+    from places.models import Place
     xid = models.CharField(_('external id'), max_length=200, blank=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField()
@@ -321,6 +322,7 @@ class OccurrenceManager(models.Manager, OccurrenceMixin):
 
 class Occurrence(models.Model):
     """Models a particular occurrence of an event"""
+    from places.models import Place
     event = models.ForeignKey(Event, related_name='occurrences')
     place = models.ForeignKey(Place, related_name='occurrences')
     one_off_place = models.CharField(max_length=200, blank=True)
