e0c5fe3a678de667bd2569090d695eda77d2e431
Vikas Menon <menonvikas@gmail.com>
2011-06-29 00:57:17 -0400

    location based classification rules

--- abextra/pundit/tests.py
+++ abextra/pundit/tests.py
@@ -188,6 +188,7 @@ class LocationRuleTest(TestCase):
                             set(location_rule.get_abstract_category(event, None, [])))
         self.assertEqual(expected_result, calculated_result)
 
+<<<<<<< HEAD
 
 class PlaceTypeRuleTest(TestCase):
     fixtures = ['events', 'categories', 'sources',
