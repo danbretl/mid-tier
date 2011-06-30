42e80c235973147ad2a60815f8eb8fe43532c1b8
Vikas Menon <menonvikas@gmail.com>
2011-06-30 13:16:42 -0400

    adding placetype rule

---
+++ abextra/places/migrations/0006_auto__add_field_placetype_concrete_category.py
@@ -0,0 +1,95 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'PlaceType.concrete_category'
+        db.add_column('places_placetype', 'concrete_category', self.gf('django.db.models.fields.related.ForeignKey')(related_name='place_concrete', null=True, to=orm['events.Category']), keep_default=False)
+
+        # Adding M2M table for field abstract_categories on 'PlaceType'
+        db.create_table('places_placetype_abstract_categories', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('placetype', models.ForeignKey(orm['places.placetype'], null=False)),
+            ('category', models.ForeignKey(orm['events.category'], null=False))
+        ))
+        db.create_unique('places_placetype_abstract_categories', ['placetype_id', 'category_id'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'PlaceType.concrete_category'
+        db.delete_column('places_placetype', 'concrete_category_id')
+
+        # Removing M2M table for field abstract_categories on 'PlaceType'
+        db.delete_table('places_placetype_abstract_categories')
+
+
+    models = {
+        'events.category': {
+            'Meta': {'object_name': 'Category'},
+            'association_coefficient': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'category_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'color': ('django.db.models.fields.CharField', [], {'max_length': '7', 'blank': 'True'}),
+            'icon': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_associative': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'subcategories'", 'null': 'True', 'to': "orm['events.Category']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'thumb': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'places.city': {
+            'Meta': {'ordering': "('state', 'city')", 'unique_together': "(('city', 'state'),)", 'object_name': 'City'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'state': ('django.contrib.localflavor.us.models.USStateField', [], {'max_length': '2'})
+        },
+        'places.place': {
+            'Meta': {'ordering': "('title',)", 'object_name': 'Place'},
+            'abstract_categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'places_abstract'", 'null': 'True', 'to': "orm['events.Category']"}),
+            'concrete_category': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'places_concrete'", 'null': 'True', 'to': "orm['events.Category']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'image_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'nickname': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'phone': ('django.contrib.localflavor.us.models.PhoneNumberField', [], {'max_length': '20', 'blank': 'True'}),
+            'place_types': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['places.PlaceType']", 'symmetrical': 'False', 'blank': 'True'}),
+            'point': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['places.Point']"}),
+            'prefix': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'unit': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'places.placetype': {
+            'Meta': {'object_name': 'PlaceType'},
+            'abstract_categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'place_abstract'", 'null': 'True', 'to': "orm['events.Category']"}),
+            'concrete_category': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'place_concrete'", 'null': 'True', 'to': "orm['events.Category']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'places.point': {
+            'Meta': {'ordering': "('address',)", 'object_name': 'Point'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['places.City']"}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'zip': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['places']
