c501ba72122b218588200de086574a33bf0df280
Vikas Menon <menonvikas@gmail.com>
2011-07-01 12:13:01 -0400

    multiple scrape tests

--- abextra/importer/tests.py
+++ abextra/importer/tests.py
@@ -47,6 +47,7 @@ Building a simple test_scrape.feed
 -------------------------------------------------
 """
 from django.core.management import call_command
+from events.models import Event, Occurrence
 
 class MultiImportTest(TestCase):
     """
@@ -54,10 +55,28 @@ class MultiImportTest(TestCase):
     # example usage: call_command('my_command', 'foo', bar='baz')
     fixtures = ['categories', 'sources', 'auth', 'external_categories_all']
 
+    def assert_event_counts(self):
+        calculated_count = len(Event.objects.all())
+        # (This could be a script that returns the number of events in the scrape feed
+        # hardcoding to 1 for now.
+        expected_count = 1
+        self.assertEqual(calculated_count, expected_count)
+
+    def assert_occurrence_counts(self, event, num):
+        # This test should be made more robust (like for only a particular event
+        calculated_count = len(Occurrence.objects.filter(event=event))
+        self.assertEqual(calculated_count, num)
+
+
     def test_multiImport(self):
         # Create 2 scrape.tar.gz files containing almost similar information
         call_command('unpack_scrapes', 'importer/fixtures/scrape1.tar.gz')
+        self.assert_event_counts()
         # Test if command was successful
-        import ipdb; ipdb.set_trace()
+        event = Event.objects.get(title__contains='X-Men')
+        self.assert_occurrence_counts(event, 1)
         call_command('unpack_scrapes', 'importer/fixtures/scrape2.tar.gz')
         # Test is second scrape ran as expected on top of the existing scrape
+        self.assert_event_counts()
+        self.assert_occurrence_counts(event, 2)
+
