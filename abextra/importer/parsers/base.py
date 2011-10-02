25a4ed31a28658573b237f91ef05d458f8b9a179
Pasha Katsev <pkatsev@gmail.com>
2011-10-01 22:59:21 -0400

    cache optimizations for base parser

--- abextra/importer/parsers/base.py
+++ abextra/importer/parsers/base.py
@@ -1,12 +1,11 @@
-import os
 import logging
 from itertools import chain
 from collections import namedtuple
-from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 
 class BaseParser(object):
     logger = logging.getLogger('importer.parser')
+    model_form = None
+    fields = []
 
     def __init__(self):
         self.model = self.model_form._meta.model
@@ -15,31 +14,28 @@ class BaseParser(object):
 
     def parse(self, data):
         form_data = self.parse_form_data(data, {})
+        # create cache key
+        key = self.cache_key(form_data)
+        # try to get from cache
+        created, instance = False, self.cache.get(key)
 
-        # try to create and validate form
-        file_data = self.parse_file_data(data, {})
-        form = self.model_form(data=form_data, files=file_data)
-        valid = form.is_valid()
-        if valid:
-            # create cache key
-            key = self.cache_key(form.cleaned_data)
-            # try cache
-            created, instance = False, self.cache.get(key)
-            # if not in the cache, save the form
-            if not instance:
+        # if cache miss, create or get from db
+        if not instance:
+            # try to create and validate form
+            file_data = self.parse_file_data(data, {})
+            form = self.model_form(data=form_data, files=file_data)
+            if form.is_valid():
                 created, instance = True, form.save(commit=True)
-                self.cache[key] = instance
-        else:
-            # could be invalid, because already exists in db
-            flat_errors = chain.from_iterable(form.errors.itervalues())
-            if any('already exists' in e for e in flat_errors):
-                key_dict = dict((f, getattr(form.instance, f)) for f in form.fields.iterkeys())
-                key = self.cache_key(key_dict)
-                created, instance = False, self.model.objects.get(**key._asdict())
-            # form is invalid due to data validation reasons
             else:
-                self.logger.error(form.errors)
-                created, instance = False, None
+                # could be invalid, because already exists in db
+                flat_errors = chain.from_iterable(form.errors.itervalues())
+                if any('already exists' in e.lower() for e in flat_errors):
+                    attrs = dict((f, getattr(form.instance, f)) for f in form.fields.iterkeys())
+                    created, instance = False, self.model.objects.get(**attrs)
+                # form is invalid due to bad data, create nothing
+                else:
+                    self.logger.error(form.errors)
+                    created, instance = False, None
 
         # if we have an instance, do a post parse
         if instance:
@@ -52,26 +48,14 @@ class BaseParser(object):
         return created, instance 
 
     def cache_key(self, form_data):
-        try:
-            kwargs = dict((f, form_data[f]) for f in self.fields)
-        except KeyError:
-            self.logger.warn("Can't create key given %s" % str(form_data))
-            return None
-        else:
-            return self.KeyTuple(**kwargs)
+        sig_fields = self.fields or form_data.keys()
+        tup = tuple(form_data.get(f) for f in sorted(sig_fields))
+        return hash(tup)
 
-    def parse_form_data(self, obj_dict, form_data={}):
+    def parse_form_data(self, obj_dict, form_data):
         raise NotImplementedError()
 
     def parse_file_data(self, data, file_data):
-        # images = data.get('images')
-        # if images:
-            # image = images[0]
-            # path = os.path.join(settings.SCRAPE_FEED_PATH, settings.SCRAPE_IMAGES_PATH, image['path'])
-            # with open(path, 'rb') as f:
-                # filename = os.path.split(f.name)[1]
-                # file_data['image'] = SimpleUploadedFile(filename, f.read())
-        # return file_data
         pass
 
     def post_parse(self, obj_dict, instance):
