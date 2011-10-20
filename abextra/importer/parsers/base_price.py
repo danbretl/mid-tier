57538c2f98ba70f4441af3dca8b8964c9493f1d9
Pasha Katsev <pkatsev@gmail.com>
2011-10-20 03:39:23 -0400

    removing strange bases

--- abextra/importer/parsers/base_price.py
+++
@@ -1,9 +0,0 @@
-from importer.parsers.base import BaseAdapter
-from prices.forms import PriceImportForm
-
-class PriceAdapter(BaseAdapter):
-    model_form = PriceImportForm
-    fields = ['occurrence', 'quantity']
-
-    def adapt_form_data(self, data, form_data):
-        raise NotImplementedError()
