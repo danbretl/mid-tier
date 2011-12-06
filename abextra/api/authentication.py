d6bda563950ebd13525fa372459919feb8c7ecf9
Pasha Katsev <pkatsev@gmail.com>
2011-12-05 19:18:26 -0500

    #72: removed relaxed email/username failover from EmailAuthBackend

--- abextra/api/authentication.py
+++ abextra/api/authentication.py
@@ -103,18 +103,10 @@ class BasicBackend:
 
 class EmailBackend(BasicBackend):
     def authenticate(self, username=None, password=None):
-        #If username is an email address, then try to pull it up
-        if email_re.search(username):
-            try:
-                user = User.objects.get(email=username)
-            except User.DoesNotExist:
-                return None
-        else:
-            #We have a non-email address username we should try username
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                return None
+        try:
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            return None
         if user.check_password(password):
             return user
 
